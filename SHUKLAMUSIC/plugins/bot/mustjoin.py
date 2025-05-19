3fb0130930729dfc43d48a88e6dbfd2f1a439714
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-05-19 19:05:40 +0530

    Update mustjoin.py

--- SHUKLAMUSIC/plugins/bot/mustjoin.py
+++ SHUKLAMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from SHUKLAMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "drxgiveaway"
+MUST_JOIN = "drxgiveway"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
