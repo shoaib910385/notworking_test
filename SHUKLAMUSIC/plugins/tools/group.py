e91d049380c16893d8a3054fb780b60017dd75eb
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-06-07 23:53:22 +0530

    Update group.py

--- SHUKLAMUSIC/plugins/tools/group.py
+++ SHUKLAMUSIC/plugins/tools/group.py
@@ -14,7 +14,7 @@ async def brah2(_, msg):
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
 async def brah3(app :app, message:Message):
-           text = f"<blockquote>{message.from_user.mention} \n**๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ ᴛᴏ : ** </blockquote>"
+           text = f"{message.from_user.mention} \n\n**<blockquote>๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ ᴛᴏ :</blockquote> **\n** "
            x = 0
            for user in message.video_chat_members_invited.users:
              try:
