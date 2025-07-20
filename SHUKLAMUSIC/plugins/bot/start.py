518cd057cc5e1fb4ad87aa1ca27b93515f07137b
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-07-20 15:29:02 +0530

    Update start.py

--- SHUKLAMUSIC/plugins/bot/start.py
+++ SHUKLAMUSIC/plugins/bot/start.py
@@ -1,7 +1,7 @@
 import time
 import random
 import asyncio
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
